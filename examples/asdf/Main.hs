0fd34c13e2e5eebd254e1c700d49edd3d86a2063
cjmackey <cjmackey@stwing.upenn.edu>
2012-07-17 06:58:56 -0400

    adding incomplete example

---
+++ examples/asdf/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Asdf
+-- import Haste (eval)
+
+main = do
+  Asdf.update "rootvar" "someid"
+
+
---
+++ examples/asdf/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+templhs-compiler Asdf.templ.html -o Asdf.hs
+hastec Main.hs
